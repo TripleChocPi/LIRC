c59fe226f62b31f346f0132b22f61175f2f4bd76
Rodger Benham <rodger.benham@gmail.com>
2013-10-07 00:36:51 +1100

    Client tree struct defined

--- client.h
+++ client.h
@@ -1,18 +1,25 @@
 #ifndef CLIENT_H
 #define CLIENT_H
-#include "lirc.h"
 
-typedef struct LIRCClientData_struct
-{
-  char* client_name;
-  int* ip_address;
-} LIRCClientData;
+#include "lirc.h"
+#include "settings.h"
+#include "client.h"
 
 typedef struct LIRCClientNode_struct
 {
-  LIRCClientData* data;
-  struct LIRCClient_struct* prev_client;
-  struct LIRCClient_struct* next_client;
+  int socket;
+  char *nick, *user;
+
+  struct LIRCClientNode_struct *left_tree;
+  struct LIRCClientNode_struct *right_tree;
 } LIRCClientNode;
+
+void lirc_client_meta_init();
+void lirc_client_connect(int socket, char* nick, char* user);
+void lirc_client_disconnect(int socket);
+LIRCClientNode* lirc_client_search(int socket);
+
+static void lirc_client_insert(LIRCClientNode *client);
+static void lirc_client_delete(LIRCClientNode *client);
 
 #endif
